edee14eafb5ef7ffbbb9f982f43458423a76a15c
Zayn074 <hao.zay074@gmail.com>
2026-08-05 17:51:37 +0800

    新增-用户登录-新增登录拦截器

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,40 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1.获取session
+        HttpSession session = request.getSession();
+        //2.获取session用户
+        User user = (User) session.getAttribute("user");
+        //3.判断用户是否存在
+        if (user == null){
+            //4.不存在拦截
+            response.setStatus(401);
+            return false;
+        }
+
+        //5.存在，将用户信息保存到ThreadLocal
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        UserHolder.saveUser(userDTO);
+        //6.放行
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
