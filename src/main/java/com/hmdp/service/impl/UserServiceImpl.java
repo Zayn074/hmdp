7e5c8039e79d8ab4e6cacf15510d6f36ff7acad6
Zayn074 <hao.zay074@gmail.com>
2026-08-05 15:33:29 +0800

    新增-用户登录-发送登录验证码

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,20 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.log.Log;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Random;
 
 /**
  * <p>
@@ -14,7 +24,24 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //1.校验手机号
+        if (!RegexUtils.isPhoneInvalid(phone)) {
+            //2.不符合返回异常
+            return Result.fail("手机号格式错误");
+        }
+        //3.符合，生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        //4.保存验证码到session
+        session.setAttribute("code",code);
+        //5.发送验证码
+        log.debug("发送验证码:"+code);
+        //6.返回ok
+        return Result.ok();
+    }
 }
